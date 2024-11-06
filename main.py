a1ff0ba2f47447bd9f859d8438bad323e93adc51
israellima03 <israelimacondori32@gmail.com>
2024-11-05 23:44:10 -0400

    seguimos implementando la base de datos

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import sqlite3
 con = sqlite3.connect("crud.db")
+
+cur = con.cursos()
+cur.execute("""
+CREATE TABLE peliculas(id INT PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT,
+            anio INTEGER, puntacion INTEGER)""")
