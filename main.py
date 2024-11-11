f5f60adf5c6395d8624b659156ce7e845b61a11c
israellima03 <israelimacondori32@gmail.com>
2024-11-10 23:03:33 -0400

    volvemos creando un enterno virtual

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sqlite3
 con = sqlite3.connect("crud.db")
 
-cur = con.cursos()
-cur.execute("""
-CREATE TABLE peliculas(id INT PRIMARY KEY AUTOINCREMENT,
-            titulo TEXT,
-            anio INTEGER, puntacion INTEGER)""")
+cur = con.cursor()
+
+valores = ("Rambo",1999,8)
+res=cur.execute("SELECT * FROM peliculas where id=547")
+print(res.fetchone())
