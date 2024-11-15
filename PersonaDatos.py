302f178fb1e01c4f36841fb6f580280d72d68373
israellima03 <israelimacondori32@gmail.com>
2024-11-14 23:57:34 -0400

    ya casi culminando el proyecto pero falta CATACORAAAAAAA

--- PersonaDatos.py
+++ PersonaDatos.py
@@ -1,13 +1,13 @@
 import conexion as con
 
 def save(persona):
+
     persona=dict(persona)
-    return tuple(persona.keys())
     try:
         db = con.conectar()
         cursor=db.cursor()
-        columnas= persona.keys()
-        valores= persona.values()
+        columnas= tuple(persona.keys())
+        valores= tuple(persona.values())
         sql="""
         INSERT INTO personas{campos} VALUES(?,?,?,?,?,?)
         """.format(campos=columnas)
@@ -15,12 +15,39 @@ def save(persona):
         creada=cursor.rowcount>0
         db.commit()
         if creada:
+            cursor.close()
+            db.close()
             return {"respuesta":creada,"mensaje":"Persona registrada"}
         else:
+            cursor.close()
+            db.close()
             return {"respuesta":creada,"mensaje":"No se logro registrar a la persona"}
     except Exception as ex: 
-        
-        return {"respuesta":False,"mensaje":str(ex)}
-    finally:
+        if "UNIQUE" in str(ex) and "correo" in str (ex):
+            mensaje = "Ya existe una persona con ese correo"
+        elif "UNIQUE" in str(ex) and "ci" in str (ex): 
+            mensaje = "Ya existe una persona con ese ci"
+        else:
+            mensaje = str(ex)
+        cursor.close()
+        db.close()    
+        return {"respuesta":False,"mensaje": mensaje}
+
+def findAll():
+    try:
+        db = con.conectar()
+        cursor=db.cursor()
+        cursor.execute("SELECT * FROM personas")
+        personas = cursor.fetchall()
+        if personas:
+            cursor.close()
+            db.close()
+            return {"respuesta":True,"personas":personas,"mensaje":"Listado OK"}
+        else:
+            cursor.close()
+            db.close()
+            return {"respuesta":False,"personas":personas,"mensaje":"No hay personas registradas"}
+    except Exception as ex:
         cursor.close()
         db.close()
+        return {"respuesta":False,"mensaje":str(ex)}
--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import PersonaDatos as per
 
-persona ={
-         "dni":"1233",
-         "nombre":"Camilo",
-         "edad":14,
-         "apellido":"Serna",
-         "direccion":"Calle 45",
-         "correo":"c@c.co"
-         }
-res=per.save(persona)
-print(res)
+res = per.findAll()
+print(res.get('personas'))
